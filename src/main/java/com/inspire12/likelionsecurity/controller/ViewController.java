b7c720b7d3c150691c7eff1fce972699a7e7bf5c
inspire12 <ox4443@naver.com>
2025-04-21 10:09:08 +0900

    [feature/security-session-signup] feat(security-session-signup): trail slash 처리

--- src/main/java/com/inspire12/likelionsecurity/controller/ViewController.java
+++ src/main/java/com/inspire12/likelionsecurity/controller/ViewController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ViewController {
-    @GetMapping("")
+
+    @GetMapping
     public String index(Model model) {
         UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("username", principal.getUsername());
