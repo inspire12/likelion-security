359d6022d51ee3b8de302c6787915a059f26e6a2
inspire12 <ox4443@naver.com>
2025-04-22 08:55:04 +0900

    [feature/security-token] feat(security-token): excludedUrl 처리

--- src/main/java/com/inspire12/likelionsecurity/infrastructure/security/filter/JwtFilter.java
+++ src/main/java/com/inspire12/likelionsecurity/infrastructure/security/filter/JwtFilter.java
@@ -20,7 +20,7 @@ public class JwtFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-    private final List<String> excludeUrls = List.of("/api/security/**", "/api/security/login", "/login", "/api/security/signup", "/signup");
+    private final List<String> excludeUrls = List.of("/api/security");
 
     public JwtFilter(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
@@ -32,7 +32,7 @@ public class JwtFilter extends OncePerRequestFilter {
         String uri = request.getRequestURI();
 
         // 제외 URL 검사
-        if (uri.startsWith("/api/security")) {
+        if (excludeUrls.stream().anyMatch(uri::startsWith)) {
             filterChain.doFilter(request, response);
             return;
         }
