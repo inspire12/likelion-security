edee9d39835c904e8d2385176cf804b7e49dd05f
inspire12 <ox4443@naver.com>
2025-04-26 07:42:11 +0900

    [feature/security-oauth2-kakao] feat(security-oauth2-kakao): kakao 로그인 추가

--- src/main/java/com/inspire12/likelionsecurity/infrastructure/security/CustomOAuth2UserService.java
+++ src/main/java/com/inspire12/likelionsecurity/infrastructure/security/CustomOAuth2UserService.java
@@ -11,6 +11,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
@@ -26,12 +27,20 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
         String provider = userRequest.getClientRegistration().getRegistrationId();
-        String email = oAuth2User.getAttribute("email");
+        String principle = "";
+        String nameAttributeKey = "email";
+        if ("KAKAO".equalsIgnoreCase(provider)) {
+            principle = Objects.requireNonNull(oAuth2User.getAttribute("id")).toString();
+            nameAttributeKey = "id";
+        } else if("GOOGLE".equalsIgnoreCase(provider)) {
+            principle = oAuth2User.getAttribute("email");
+        }
+
 
         // 회원정보 저장/업데이트
-        UserEntity user = userRepository.findByUsername(email);
+        UserEntity user = userRepository.findByUsername(principle);
         if (user == null) {
-            user = new UserEntity(email, "", List.of(new SimpleGrantedAuthority("ROLE_USER")));
+            user = new UserEntity(principle, "", List.of(new SimpleGrantedAuthority("ROLE_USER")));
             userRepository.save(user);
         }
 //        SecurityContextHolder.getContext().setAuthentication(new OAuth2AuthenticationToken(oAuth2User, oAuth2User.getAuthorities(), provider));
@@ -39,7 +48,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         return new DefaultOAuth2User(
             List.of(new SimpleGrantedAuthority("ROLE_USER")),
             oAuth2User.getAttributes(),
-            "email"
+            nameAttributeKey
         );
     }
 }
